f8f9943cae734c667d1e467ca5758eb1c50d6979
Adolph <517323863@qq.com>
2022-03-31 15:52:57 +0800

    second commit

--- src/main/java/com/wxs/oes/OnlineExamSystemApplication.java
+++ src/main/java/com/wxs/oes/OnlineExamSystemApplication.java
@@ -1,9 +1,11 @@
 package com.wxs.oes;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.wxs.oes.mapper")
 public class OnlineExamSystemApplication {
 
     public static void main(String[] args) {
