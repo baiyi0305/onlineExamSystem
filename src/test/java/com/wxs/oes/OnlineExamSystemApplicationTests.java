47c32221e5b29a8d8b12150b39de5f556f6c2d3b
baiyi0305 <63993036+baiyi0305@users.noreply.github.com>
2022-04-03 22:55:15 +0800

    Update OnlineExamSystemApplicationTests.java

--- src/test/java/com/wxs/oes/OnlineExamSystemApplicationTests.java
+++ src/test/java/com/wxs/oes/OnlineExamSystemApplicationTests.java
@@ -9,13 +9,4 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class OnlineExamSystemApplicationTests {
 
-    @Autowired
-    AdminMapper adminMapper;
-
-    @Test
-    void contextLoads() {
-        Admin admin = adminMapper.selectById(9527);
-        System.out.println(admin);
-    }
-
 }
